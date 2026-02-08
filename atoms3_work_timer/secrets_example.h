2872529e378314c15b27645ed830cd8c2304d3e3
Binkai <kaikuhfeld@gmail.com>
2026-02-08 17:53:36 +0100

    Neustrukturierung

--- atoms3_work_timer/secrets_example.h
+++
@@ -1,8 +0,0 @@
-// Beispiele
-#define WEBHOOK_URL_SIDE1 "https://deine.api.url"
-#define SECRET_SSID "DEINE SSID Zuhause"
-#define SECRET_PASS "Passwort Zuhause"
-#define SECRET_SSID2 "DEINE SSID Büro"
-#define SECRET_PASS2 "Passwort Büro"
-#define apiToken_SEC "API Secret Token (wenn notwendig)!"
-#define ntfy_url_SEC "http://ntfy.sh/deine_URL"
